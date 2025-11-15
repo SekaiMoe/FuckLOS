bfb5e989547105baf456ac0200503ad6f487874e
AlexLiuDev233 <wzylin11@outlook.com>
2025-11-15 21:05:31 +0800

    inital

---
+++ app/src/main/java/me/alexliudev/stopvinty/Main.java
@@ -0,0 +1,25 @@
+package me.alexliudev.stopvinty;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Main implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("android")) return;
+        XposedHelpers.findAndHookMethod(
+                "android.os.Build",
+                lpparam.classLoader,
+                "isBuildConsistent",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(true);
+                    }
+                }
+        );
+    }
+}
